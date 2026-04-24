b2aa429ce6e5fdc53f137a935fc71bda55ca1795
Ignacio-dev0 <nacho123benitez@gmail.com>
2026-04-24 15:59:16 -0300

    feat(database): agregue ON CONFLICT en el INSERT para mejores practicas

--- database/init.sql
+++ database/init.sql
@@ -11,4 +11,5 @@ INSERT INTO "members" (nombre, apellido, legajo, feature, servicio, estado) VALU
 ('Ignacio', 'Benitez', 33507, '4', 'database', 'en proceso'),
 ('Matias', 'Dieguez', 33080, '2', 'frontend', 'en proceso'),
 ('Naim', 'Guarino', 32683, '1,5', 'infra', 'en proceso'),
-('Agustín', 'Manrique', 31976, '3', 'backend', 'en proceso');
+('Agustín', 'Manrique', 31976, '3', 'backend', 'en proceso')
+ON CONFLICT (legajo) DO NOTHING;
